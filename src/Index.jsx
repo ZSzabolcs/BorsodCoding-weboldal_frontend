23f5ef91cecc5bbab9edfef9ddea8d8099965d5d
Csiger Imre Krisztián <CsigerIK@KKSZKI.LOCAL>
2025-10-21 12:24:51 +0200

    Index modul létrehozva

---
+++ src/Index.jsx
@@ -0,0 +1,25 @@
+function Index() {
+  const username = sessionStorage.getItem("username")
+  return (
+    <>
+    <h1>Üdvözöljük <span>{username}!</span></h1>
+    <h2>Híreink</h2>
+    <div className="row">
+        <div className="col-md-6 border">
+            <h3>Történetünk első játéka</h3>
+            <p>
+                Elkészült a <strong>For The Potatoe</strong> című <strong>legelső játékunk!</strong>  Kattintson a játékról nevű gombra, ha érdekli.
+            </p>
+        </div>
+        <div className="col-md-6 border">
+            <h3>Mikor lesz For The Potatoe 2?</h3>
+            <p>
+                A legelső játék készítése alatt már a folytatásai is kirajzolódott. A For The Potatoe 2 játékot 2026-ban vagy 2027-ben tervezzük elkezdeni a pénz függvényében.
+            </p>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Index
