cc3ece94b00504cbd63cf9c92e12c2c14eaa5360
Csiger Imre Krisztián <CsigerIK@KKSZKI.LOCAL>
2025-10-21 12:12:07 +0200

    Globális Navigation modul létrehozva

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-function App() {
+function Index() {
   const [count, setCount] = useState(0)
 
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ createRoot(document.getElementById('root')).render(
     <>
     <StrictMode>
     <HeaderModule />
+    <NavigationModule></NavigationModule>
     <App />
     </StrictMode>
     </>
@@ -27,6 +28,31 @@ function HeaderModule() {
   )
 }
 
-function name(params) {
-  
+function NavigationModule() {
+  return(
+    <div className="container-fluid navigation">
+        <div className="row justify-content-center text-center">
+            <div className="col-xxl-1 col-md-3 col-sm-4 p-2 mx-4">
+                <a href="index.html">
+                    Kezdőlap
+                </a>
+            </div>
+            <div className="col-xxl-1 col-md-3 col-sm-4 p-2 mx-4">
+                <a href="ajatekrol.html">
+                    A játékról
+                </a>                        
+            </div>
+            <div className="col-xxl-1 col-md-3 col-sm-4 p-2 mx-4">
+                <a href="rolunk.html">
+                    Rólunk
+                </a>                        
+            </div>
+            <div className="col-xxl-1 col-md-3 col-sm-4 p-2 mx-4">
+                <a href="kapcsolat.html">
+                    Kapcsolat
+                </a>                        
+            </div>
+        </div>
+    </div>
+  )
 }
