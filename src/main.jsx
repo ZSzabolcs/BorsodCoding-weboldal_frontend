6682d9eda5928c3fdecc5db08c6ddc5f9905e2f1
Cs12krisz <csigerkrisztian@gmail.com>
2025-11-19 19:29:19 +0100

    A weboldal ellenőrzése a modulok használatához

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,10 @@ import Index from "./Index.jsx"
 import "./index.css"
 import Navigation from './Navigation.jsx'
 import Header from './Header.jsx'
+import AJatekrol from './Ajatekrol.jsx'
+import Registration from './Registration.jsx'
+import Rolunk from './Rolunk.jsx'
+import Kapcsolat from './Kapcsolat.jsx'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.min.js"
 
@@ -19,6 +23,40 @@ if (document.getElementById("root")) {
     )
 }
 
+else if(document.getElementById("ajatekrol")){
+    createRoot(document.getElementById('ajatekrol')).render(
+    <>
+    <StrictMode>
+    <Header />
+    <Navigation/>
+    <AJatekrol/>
+    </StrictMode>
+    </>
+    )
+}
 
+else if(document.getElementById("kapcsolat")){
+    createRoot(document.getElementById('kapcsolat')).render(
+    <>
+    <StrictMode>
+    <Header />
+    <Navigation/>
+    <Kapcsolat/>
+    </StrictMode>
+    </>
+    )
+}
+
+else if(document.getElementById("rolunk")){
+    createRoot(document.getElementById('rolunk')).render(
+    <>
+    <StrictMode>
+    <Header />
+    <Navigation/>
+    <Rolunk/>
+    </StrictMode>
+    </>
+    )
+}
 
 
