11d0e6d35286d7e771c571f04fe1f230d8b13431
ZSzabolcs <90418566+ZSzabolcs@users.noreply.github.com>
2025-12-16 09:40:02 +0100

    Regisztráció megdizájnolva

---
+++ main.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { App } from './App.jsx'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.min.js"
+import "bootstrap-icons/font/bootstrap-icons.css"
+import "./css/index.css"
+
+createRoot(document.getElementById('root')).render(
+    <>
+    <StrictMode>
+        <BrowserRouter>
+            <App></App>
+        </BrowserRouter>
+    </StrictMode>
+    </>
+)
+
