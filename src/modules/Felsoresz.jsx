1c014615c69d7105b7bb9a345154d1abb816e8d8
Cs12krisz <csigerkrisztian@gmail.com>
2026-04-08 15:34:38 +0200

    Kijelentkezés gomb hozzáadása

--- src/modules/Felsoresz.jsx
+++ src/modules/Felsoresz.jsx
@@ -1,11 +1,19 @@
+import { useNavigate } from "react-router-dom";
 import Header from "./Header.jsx";
 import Navigation from "./Navigation.jsx";
+
+const backToLoginAndRegistPage = () => {
+    sessionStorage.removeItem("username")
+    localStorage.removeItem("jwt")
+    location.assign("/")
+}
 
 const Felsoresz = () => {
     return(
         <>
             <Header/>
             <Navigation/>
+            <button className="btn btn-primary" onClick={backToLoginAndRegistPage}>Kijelentkezés</button>
         </>
     )
 }
